92252e6deccbdde7d97f7fba2a92ae2d165749c5
Bob Yexley <bob@yexley.net>
2014-12-28 17:35:10 -0500

    Processing options, and checking for existence of given directories

--- process-photos.js
+++ process-photos.js
@@ -1,12 +1,15 @@
 var pkg = require("./package.json");
+
 /* node modules */
+var fs = require("fs");
 
 /* imported modules */
 var _ = require("lodash");
+var async = require("async");
+var Exif = require("exif").ExifImage;
+var GetOptions = require("node-getopt");
 var log = require("custom-logger");
 var multiline = require("multiline");
-var GetOptions = require("node-getopt");
-var Exif = require("exif").ExifImage;
 
 _.templateSettings.interpolate = /{([\s\S]+?)}/g;
 
@@ -49,7 +52,71 @@ if (!args.options.destination || (args.options.destination && args.options.desti
 
 // if we have both a source and a destination, we can proceed
 if ((args.options.source && args.options.source.length > 0) && (args.options.destination && args.options.destination.length > 0)) {
-    log.info(_.template("Processing photos in {source} and moving them to {destination}", { source: args.options.source, destination: args.options.destination }));
+    var options = {
+        source: resolvePath(args.options.source),
+        destination: resolvePath(args.options.destination)
+    };
+
+    log.info(_.template("Processing photos in {source} and moving them to {destination}", options));
+
+    main(options);
 }
 
 /* END Command line argument processing */
+
+function resolvePath (path) {
+    if (path.indexOf("~") > -1) {
+        path = path.replace("~", process.env.HOME);
+    }
+
+    return path;
+};
+
+function main (options) {
+    async.series([
+        function (next) {
+            fs.exists(options.source, function (exists) {
+                if (exists) {
+                    fs.stat(options.source, function (err, stats) {
+                        if (stats && stats.isDirectory) {
+                            next(null, true)
+                        } else {
+                            next("Source is not a directory", false);
+                        }
+                    });
+                } else {
+                    next("Source directory does not exist", false);
+                }
+            });
+        },
+        function (next) {
+            fs.exists(options.destination, function (exists) {
+                if (exists) {
+                    fs.stat(options.destination, function (err, stats) {
+                        if (stats && stats.isDirectory) {
+                            next(null, true);
+                        } else {
+                            next("Destination is not a directory", false)
+                        }
+                    })
+                } else {
+                    fs.mkdir(options.destination, 0755, function (err) {
+                        if (!err) {
+                            next(null, true);
+                        } else {
+                            next("Error creating destination directory", false);
+                        }
+                    });
+                }
+            });
+        }
+    ], function (err, results) {
+        if (_.all(results)) { // if all of the above functions completed successfully...
+            processPhotos(options);
+        }
+    });
+};
+
+function processPhotos (options) {
+    log.debug("Walking source tree and processing photos...");
+};
